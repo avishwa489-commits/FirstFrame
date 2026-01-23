a87a7111acf4eb2df96c7c57bb3afd174740c5c5
avishwa666-create <a.vishwa666@gmail.com>
2026-01-23 17:30:12 +0530

    New project Added in the frameWork

---
+++ src/test/java/abc/Practice_Test.java
@@ -0,0 +1,8 @@
+package abc;
+
+public class Practice_Test {
+	public static void main(String[] args) {
+		System.out.println("Vishwa");
+	}
+
+}
---
+++ src/test/java/abc/RealExecution_Test.java
@@ -0,0 +1,15 @@
+package abc;
+
+import org.testng.annotations.Test;
+
+public class RealExecution_Test {
+@Test
+public void newMethod_1() {
+	System.out.println("new method 1 executed");
+}
+@Test
+public void newMethod_2() {
+	System.out.println("new method 2 executed");
+}
+
+}
